58c2c584a3330f9f6dc86aed631fccc25ea50361
DarkCreeper134 <dylan.hall@inglewoodhs.school.nz>
2021-05-18 15:05:56 +1200

    Slime Ai updated to add wander

--- Sprites/Enemys/SlimeEnemy.gd
+++ Sprites/Enemys/SlimeEnemy.gd
@@ -13,12 +13,15 @@ enum {
 var velocity = Vector2.ZERO
 var knockback = Vector2.ZERO
 
+
 var state = CHASE
 
+onready var stats = $Stats
 onready var playerDetectionZone = $PlayerDetectionZone
 onready var sprite = $AnimatedSprite
 onready var hurtbox = $Hurtbox
 onready var softCollision = $SoftCollision
+onready var wanderController = $WanderController
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
@@ -28,25 +31,45 @@ func _physics_process(delta):
 		IDLE:
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 			seek_player()
+			check_state()
 		
 		WANDER:
-			pass
+			seek_player()
+			check_state()
+			
+			var direction = global_position.direction_to(wanderController.target_position)
+			velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
 		
 		CHASE:
 			var player = playerDetectionZone.player
 			if player != null:
-				var direction = (player.global_position - global_position).normalized()
-				velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+				var direction = global_position.direction_to(player.global_position)
+				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
 			else:
 				state = IDLE
+			sprite.flip_h = velocity.x < 0
 	
 	if softCollision.is_colliding():
 		velocity += softCollision.get_push_vector() * delta * 400
 	velocity = move_and_slide(velocity)
-	
+
 func seek_player():
 	if playerDetectionZone.can_see_player():
 		state = CHASE
+		
+func check_state():
+	if wanderController.get_time_left() == 0:
+				state = pick_random_state([IDLE, WANDER])
+				wanderController.start_wander_timer(rand_range(1,3))
+
+func pick_random_state(state_list):
+	state_list.shuffle()
+	return state_list.pop_front()
+
+func _on_Stats_no_health():
+	queue_free()
 
 func _on_HurtBox_area_entered(area):
+	stats.health -= area.damage
 	knockback = area.knockback_vector * 120
+	hurtbox.create_hit_effect()
