f26163d8205dab411ce38b016cbac7c43344d1a4
DarkCreeper134 <dylan.hall@inglewoodhs.school.nz>
2021-06-04 09:04:14 +1200

    Fixed healthbar to update current health properly

--- Sprites/HealthBar.gd
+++ Sprites/HealthBar.gd
@@ -1,23 +1,28 @@
 extends Control
 
-var hearts = 4.0 setget set_hearts
-var max_hearts = 4.0 setget set_max_hearts
+var hearts = 4 setget set_hearts
+var max_hearts = 4 setget set_max_hearts
 
 onready var heartUIFull = $Full
 onready var heartUIEmpty = $Empty
-onready var healthPercent = (float(hearts) / float(max_hearts))
+onready var floatHealth = float(hearts)
+onready var floatMaxHealth = float(max_hearts)
+onready var healthPercent = floatHealth / floatMaxHealth
 
 func set_hearts(value):
 	hearts = clamp(value, 0, max_hearts)
-	var healthPercent = hearts / max_hearts
+	floatHealth = float(hearts)
+	healthPercent = floatHealth/floatMaxHealth
 	if heartUIFull != null:
-		heartUIFull.rect_size.x = int(healthPercent) * 160
+		heartUIFull.rect_size.x = healthPercent * 160
 
 func set_max_hearts(value):
 	max_hearts = max(value, 1)
 	self.hearts = min(hearts, max_hearts)
+	floatHealth = float(hearts)
+	floatMaxHealth = float(max_hearts)
 	if heartUIFull != null:
-		heartUIFull.rect_size.x = (hearts/max_hearts) * 160
+		heartUIFull.rect_size.x = healthPercent * 160
 
 func _ready():
 	self.max_hearts = PlayerStats.max_health
